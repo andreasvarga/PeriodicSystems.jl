02df45e3832f82e3ad374d69c058fb0c76a12c65
DSVarga <varga.andreas@gmail.com>
2022-05-06 18:40:04 +0200

    More tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,9 @@ using Test, PeriodicSystems
 
 @testset "Test PeriodicSystems" begin
 # test constructors, basic tools
-include("test_psutils.jl")
-include("test_conversions.jl")
-#include("test_pslifting.jl")
+#include("test_psutils.jl")
+#include("test_conversions.jl")
+include("test_pslifting.jl")
 end
 
 end
